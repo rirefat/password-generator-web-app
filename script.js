45e2cf322218fafa1cafb476f89095ed8e65079c
rirefat <rirefat.official@gmail.com>
2023-01-09 19:25:17 +0600

    done js script

---
+++ script.js
@@ -0,0 +1,12 @@
+const getPassword=()=>{
+    let chars = "0123456789zxcvbnmlkjhgfdsaqwertyuiopQWERTYUIOPLKJHGFDSAZXCVBNM!@#$%^&*()_+?><:{}[]";
+    let passwordLength = 16;
+    let password ="";
+
+    for(let i = 0; i<=passwordLength; i++){
+        let randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber+1);
+    }
+
+    document.getElementById("password").value = password;
+}
