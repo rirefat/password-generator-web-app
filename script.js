58fc92d3588055ff20f17877341d3c14d0f0aae7
rirefat <rirefat.official@gmail.com>
2023-01-12 19:37:34 +0600

    slight modification

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Generating Password
 const getPassword=()=>{
     let chars = "0123456789zxcvbnmlkjhgfdsaqwertyuiopQWERTYUIOPLKJHGFDSAZXCVBNM!@#$%^&*()_+?><:{}[]";
     let passwordLength = 16;
@@ -11,6 +12,15 @@ const getPassword=()=>{
     document.getElementById("password").value = password;
 }
 
+// Clear Generated Password
 const clearPassword =()=>{
     document.getElementById("password").value = "";
+}
+
+// Copy Generated Password
+const copyPassword =()=>{
+    // let input = document.getElementById("password");
+    // input.select();
+    // document.execCommand("copy");
+
 }
