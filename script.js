1022189ec5c9f5b7ddc5a0ba8e366f9434e7745c
rirefat <rirefat.official@gmail.com>
2023-01-14 12:22:44 +0600

    add pass range slider

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Generating Password
 const getPassword=()=>{
     let chars = "0123456789zxcvbnmlkjhgfdsaqwertyuiopQWERTYUIOPLKJHGFDSAZXCVBNM!@#$%^&*()_+?><:{}[]";
-    let passwordLength = document.getElementById("digit_number").value - 1;
+    let passwordLength = document.getElementById("range").value-1;
     console.log(passwordLength)
     let password ="";
+    // document.getElementById("range_value").innerText=passwordLength;
+    console.log(passwordLength)
 
     for(let i = 0; i<=passwordLength; i++){
         let randomNumber = Math.floor(Math.random() * chars.length);
@@ -13,10 +15,18 @@ const getPassword=()=>{
     document.getElementById("password").value = password;
 }
 
+// Range value change
+const changeRange = (value) =>{
+    let passwordLength = value;
+    document.getElementById("range_value").innerText=passwordLength;
+}
+
 // Clear Generated Password
 const clearPassword =()=>{
     document.getElementById("password").value = "";
     document.getElementById("digit_number").value = "";
+    document.getElementById("range").value = 0;
+    document.getElementById("range_value").innerText=0;
 }
 
 // Copy Generated Password
